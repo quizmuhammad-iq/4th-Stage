378f3ba005efa8a0507bc87bce4854f6fda364ee
quizmuhammad-iq <muhammad.sabbar.alsalmany@gmail.com>
2026-07-29 23:06:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ministquiz-v1.1';
+const CACHE_NAME = 'ministquiz-v1.2';
 // قائمة الملفات اللي نريد نحفظها بالذاكرة (بدون سلاش وبدون ملف الأخطاء الممسوح)
 const urlsToCache = [
     './',
