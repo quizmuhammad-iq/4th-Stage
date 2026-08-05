05d85b3a753f6a5eaee13a7223410747ac29738a
quizmuhammad-iq <muhammad.sabbar.alsalmany@gmail.com>
2026-08-05 21:44:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -20,8 +20,13 @@ self.addEventListener('install', event => {
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
+        fetch(event.request).then(response => {
+            return caches.open(CACHE_NAME).then(cache => {
+                cache.put(event.request, response.clone());
+                return response;
+            });
+        }).catch(() => {
+            return caches.match(event.request);
         })
     );
 });
